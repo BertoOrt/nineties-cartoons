8fd1d162c8821784eff6889aeef05c8c5bd1cd5a
Roberto Ortega <berto.ort@gmail.com>
2015-08-12 17:26:00 -0600

    filtering

---
+++ cartoons.js
@@ -0,0 +1,8 @@
+var cartoons = [
+  {name: "Rocko's Modern Life", image: 'https://upload.wikimedia.org/wikipedia/en/0/01/Rocko_title_card.jpg', score: 2},
+  {name: "Pokemon", image: 'http://3.bp.blogspot.com/_2reYE43DHZE/S6fvYKmxQ4I/AAAAAAAAF-I/uk-Af38lhzQ/s320/pokemon_title.jpg', score: 5},
+  {name: "Animaniacs", image: 'http://pre14.deviantart.net/5c7c/th/pre/i/2015/204/4/3/animaniacs_logo_complete_with_shading_by_renardfox-d92itfx.png', score: 2},
+  {name: "Doug", image: 'http://d3vz5f63gip8jk.cloudfront.net/fb_100055_poster.png', score: -1},
+  {name: 'Hey Arnold!', image: 'http://multiredtv.com/wp-content/uploads/2014/09/Oye+Arnold.jpg', score: 0},
+  {name: "Rugrats", image: 'http://www.cool2bkids.com/wp-content/uploads/2015/03/Rugrats.jpg', score: 3}
+]
