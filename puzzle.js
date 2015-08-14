f66f78c05db7c67535e8c251743f65201e08f0dd
Roberto Ortega <berto.ort@gmail.com>
2015-08-14 15:06:13 -0600

    ready

--- puzzle.js
+++ puzzle.js
@@ -1,57 +1,37 @@
 var floors = 100;
-var eggs = 2;
-var breakingPoint = 16;
-var foundBreakingPoint;
+var result = {};
 
-var methodOne = function () {
-  var checks = 0;
-  for (var i = eggs; i <= floors; i+=eggs) {
-    checks++
-    if (i >= breakingPoint) {
-      findPoint(i)
-      break;
-    }
-  }
-  function findPoint(i) {
-    for (var j = 0; j <= eggs; j++) {
-      if (i - j === breakingPoint) {
-        foundBreakingPoint = i - j;
-        checks++
-      }
-    }
-  }
-  console.log(breakingPoint, foundBreakingPoint, checks);
-}
-
-var methodTwo = function (by, floor) {
-  var lastGoodPoint;
-  var goBy = by;
-  var checks = 0;
-  if (floor >= breakingPoint) {
-    findPoint(i)
+function findPoint(by, floor, checks, breakingPoint) {
+  checks++
+  if (floor + 1 === breakingPoint) {
+    var foundBreakingPoint = floor + 1;
+    result[by] = result[by] || []
+    result[by].push({breakingPoint: breakingPoint, checks: checks})
   } else {
-
+    findPoint(by, floor + 1, checks, breakingPoint)
   }
-  for (var i = goBy; i <= floors; i+=goBy) {
-    checks++
-    if (i >= breakingPoint) {
-
-      break;
-    }
-    lastGoodPoint = i;
-  }
-  function findPoint(i) {
-    for (var j = 0; j <= i - lastGoodPoint; j++) {
-      checks++
-      if (lastGoodPoint + j === breakingPoint) {
-        foundBreakingPoint = lastGoodPoint + j;
-        break;
-      }
-    }
-  }
-  console.log(breakingPoint, foundBreakingPoint, checks);
 }
 
+var findFloor = function (by, floor, checks, breakingPoint) {
+  checks++
+  if (floor + by >= breakingPoint) {
+    findPoint(by, floor, checks, breakingPoint)
+  } else {
+    findFloor(by, floor + by, checks, breakingPoint)
+  }
+}
 
+for (var x = 1; x <= floors; x++) {
+  for (var i = 1; i <= floors; i++) {
+    findFloor(x, 0, 0, i);
+  }
+}
 
-methodTwo();
+var averages = {};
+for (var key in result) {
+  averages[key] = (result[key].reduce(function (curr, prev) {
+    return curr + prev.checks
+  }, 0) / floors)
+}
+
+console.log(100/14)
